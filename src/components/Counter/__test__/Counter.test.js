df8a1d9e3ed179379879d8483065a5932c025193
afroze9 <afrozeamjad@gmail.com>
2022-03-25 10:54:30 +0500

    Init + header test

---
+++ src/components/Counter/__test__/Counter.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Counter from '../Counter';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+test('header renders with correct text', () => {
+  render(<Counter />);
+  const headerEl = screen.getByTestId('header');
+  expect(headerEl.textContent).toBe('My Counter');
+})
